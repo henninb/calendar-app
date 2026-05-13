a6fb04b37e3115d11313aa7d9b55424a338effb9
GrapheneOS Build <henninb@msn.com>
2026-05-13 05:40:04 -0500

    wip

--- frontend-nextgen/components/tasks/TaskList.tsx
+++ frontend-nextgen/components/tasks/TaskList.tsx
@@ -452,7 +452,7 @@ export default function TaskList() {
   }, [grouped, focusedTaskId, patchTask, toggleExpand])
 
   return (
-    <div className="max-w-4xl mx-auto">
+    <div className="max-w-6xl mx-auto">
       <TaskToolbar
         searchQuery={searchQuery}
         onSearch={setSearchQuery}
